591226273d65eae860febec2d9e0ba36f0918a3f
zhuchao <2580500887@qq.com>
2019-05-31 08:57:00 +0800

    索引器

---
+++ ConsoleApp32/TestIndexer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp32
+{
+    class TestIndexer
+    {
+        private string[] namelist = new string[size];
+
+        static public int size = 10;
+
+        public TestIndexer() {
+            for (int i = 0; i < size; i++)
+            namelist[i] = "N.A.";
+        }
+
+        public string this[int index] {
+            get {
+                string tmp;
+                if (index >= 0 && index <= size - 1) {
+                    tmp = namelist[index];
+                 }
+                else
+                {
+                    tmp = "";
+                }
+                return (tmp);
+            }
+
+            set {
+                if (index >= 0 && index <= size - 1) {
+                    namelist[index] = value;
+                }              
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            TestIndexer names = new TestIndexer();
+            names[0] = "Zara";
+            names[1] = "Riz";
+            names[2] = "Nuha";
+            names[3] = "Asif";
+            names[4] = "Davinder";
+            names[5] = "Sunil";
+            names[6] = "Rubic";
+            for (int i = 0; i < TestIndexer.size; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
