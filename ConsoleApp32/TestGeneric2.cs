b757441f8ee76cd112c602ecc06b8cee127be9cc
zhuchao <2580500887@qq.com>
2019-05-31 17:28:45 +0800

    泛型委托

---
+++ ConsoleApp32/TestGeneric2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+delegate T NumberChanger<T>(T n);
+namespace ConsoleApp32
+{
+    class TestGeneric2
+    {
+        static int num = 10;
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+        public static int getNum()
+        {
+            return num;
+        }
+
+        static void Main(string[] args)
+        {
+            // 创建委托实例
+            NumberChanger<int> nc1 = new NumberChanger<int>(AddNum);
+            NumberChanger<int> nc2 = new NumberChanger<int>(MultNum);
+            // 使用委托对象调用方法
+            nc1(25);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            nc2(5);
+            Console.WriteLine("Value of Num: {0}", getNum());
+            Console.ReadKey();
+        }
+    }
+}
+
