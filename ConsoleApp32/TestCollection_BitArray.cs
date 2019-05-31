1311e4d24261e9b7a26777f180e6db6c05d94aed
zhuchao <2580500887@qq.com>
2019-05-31 17:16:47 +0800

    点阵列

---
+++ ConsoleApp32/TestCollection_BitArray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp32
+{
+    class TestCollection_BitArray
+    {
+        static void Main(string[] args)
+        {
+            // 创建两个大小为 8 的点阵列
+            BitArray ba1 = new BitArray(8);
+            BitArray ba2 = new BitArray(8);
+            byte[] a = { 60 };
+            byte[] b = { 13 };
+
+            // 把值 60 和 13 存储到点阵列中
+            ba1 = new BitArray(a);
+            ba2 = new BitArray(b);
+
+            // ba1 的内容
+            Console.WriteLine("Bit array ba1: 60");
+            for (int i = 0; i < ba1.Count; i++)
+            {
+                Console.Write("{0, -6} ", ba1[i]);
+            }
+            Console.WriteLine();
+
+            // ba2 的内容
+            Console.WriteLine("Bit array ba2: 13");
+            for (int i = 0; i < ba2.Count; i++)
+            {
+                Console.Write("{0, -6} ", ba2[i]);
+            }
+            Console.WriteLine();
+
+
+            BitArray ba3 = new BitArray(8);
+            ba3 = ba1.And(ba2);
+
+            // ba3 的内容
+            Console.WriteLine("Bit array ba3 after AND operation: 12");
+            for (int i = 0; i < ba3.Count; i++)
+            {
+                Console.Write("{0, -6} ", ba3[i]);
+            }
+            Console.WriteLine();
+
+            ba3 = ba1.Or(ba2);
+            // ba3 的内容
+            Console.WriteLine("Bit array ba3 after OR operation: 61");
+            for (int i = 0; i < ba3.Count; i++)
+            {
+                Console.Write("{0, -6} ", ba3[i]);
+            }
+            Console.WriteLine();
+
+            Console.ReadKey();
+        }
+    }
+}
