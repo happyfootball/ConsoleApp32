ddf6f77e95f24d42e178b4d5d25993be471ef567
zhuchao <2580500887@qq.com>
2019-05-31 17:26:44 +0800

    泛型方法

---
+++ ConsoleApp32/TestGeneric1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp32
+{
+    class TestGeneric1
+    {
+        static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+        static void Main(string[] args)
+        {
+            int a, b;
+            char c, d;
+            a = 10;
+            b = 20;
+            c = 'I';
+            d = 'V';
+
+            // 在交换之前显示值
+            Console.WriteLine("Int values before calling swap:");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("Char values before calling swap:");
+            Console.WriteLine("c = {0}, d = {1}", c, d);
+
+            // 调用 swap
+            Swap<int>(ref a, ref b);
+            Swap<char>(ref c, ref d);
+
+            // 在交换之后显示值
+            Console.WriteLine("Int values after calling swap:");
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+            Console.WriteLine("Char values after calling swap:");
+            Console.WriteLine("c = {0}, d = {1}", c, d);
+            Console.ReadKey();
+        }
+    }
+}
