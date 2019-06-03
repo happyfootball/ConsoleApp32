e0d5e8ed4d23d46be860b15b10a99333b14993dc
zhuchao <2580500887@qq.com>
2019-06-03 08:51:09 +0800

    线程测试1

---
+++ ConsoleApp32/TestThread1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApp32
+{
+    class TestThread1
+    {
+        static void Main(string[] args)
+        {
+            Thread th = Thread.CurrentThread;
+            th.Name = "Main Thread";
+            Console.WriteLine("This is {0}", th.Name);
+            Console.ReadKey();
+        }
+    }
+}
