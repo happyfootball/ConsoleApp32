3442caba3b9cbab260f00987ecd3aca2b3092b81
zhuchao <2580500887@qq.com>
2019-06-03 08:44:24 +0800

    指针测试

---
+++ ConsoleApp32/TestPointer1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp32
+{
+    class TestPointer1
+    {
+        static void Main(string[] args)
+        {
+            int var = 20;
+            int *p = &var;
+            Console.WriteLine("Data is: {0} ", var);
+            Console.WriteLine("Address is: {0}", (int)p);
+            Console.ReadKey();
+        }
+    }
+}
