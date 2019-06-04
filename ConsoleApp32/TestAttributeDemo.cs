f5336a95bfab9b209a198bb5c796b4d9b865afe9
zhuchao <2580500887@qq.com>
2019-06-04 09:22:07 +0800

    特性的实例

---
+++ ConsoleApp32/TestAttributeDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp32
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method,AllowMultiple=true,Inherited=false)]
+    public class TestAttributeDemo:Attribute
+    {
+        private DateTime dateModified;
+        private string changes;
+        private string issues;
+
+        public TestAttributeDemo(string dateModified,string changes) {
+            this.dateModified = DateTime.Parse(dateModified);
+            this.changes = changes;
+        }
+
+        public DateTime DateModified {
+            get {
+                return dateModified;
+            }
+        }
+
+        public string Changes {
+            get {
+                return changes;
+            }
+        }
+
+        public string Issues {
+            get {
+                return issues;
+            }
+            set {
+                issues = value;
+            }
+        }
+    }
+}
