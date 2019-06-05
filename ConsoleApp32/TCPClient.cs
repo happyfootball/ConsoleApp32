08590db2fdb1d2ec345961a624be26bcdf59a80d
zhuchao <2580500887@qq.com>
2019-06-05 16:42:04 +0800

    C# 网络编程之Tcp实现客户端和服务器聊天

---
+++ ConsoleApp32/TCPClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ConsoleApp32
+{
+    class TCPClient
+    {
+        static void Main(string[] args)
+        {
+            //Parse将字符串装换为IP地址类型
+            IPAddress myIP = IPAddress.Parse("127.0.0.1");
+            //构造一个TcpClient类对象，TCP客户端
+            TcpClient client=new TcpClient();
+            //与TCP服务器连接
+            client.Connect(myIP ,6688);
+            Console.WriteLine("服务器已经连接...请输入对话内容...");
+
+
+            // 创建网络流,获取数据流
+             NetworkStream stream = client.GetStream();
+            //读数据流对象
+             StreamReader sr = new StreamReader(stream);
+            //写数据流对象
+             StreamWriter sw = new StreamWriter(stream);
+
+            while (true)
+            {
+                string msg = Console.ReadLine();
+                sw.WriteLine(msg);
+                sw.Flush();
+                Console.WriteLine("服务器："+sr.ReadLine());
+            }
+            client.Close();
+            Console.Read();
+           
+
+        }
+    }
+}
---
+++ ConsoleApp32/TCPServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApp32
+{
+    class TCPServer
+    {
+        static void Main(string[] args)
+        {
+            //parse将字符串转换为IP地址类型
+            IPAddress myIP = IPAddress.Parse("127.0.0.1");
+            //构造一个TcpListener(IP地址，端口)对象，TCP服务端
+            TcpListener myServer = new TcpListener(myIP, 6688);
+
+            //开始监听
+            myServer.Start();
+            Console.WriteLine("等待一个连接。。。");
+
+
+            //构造Tcp客户端，接受连接请求
+            TcpClient client = myServer.AcceptTcpClient();
+            Console.WriteLine("客户端已经连接。。。");
+
+            //构造NetworkStream类，该类用于获取和操作网络流
+            NetworkStream stream = client.GetStream();
+            //读数据流对象
+            StreamReader sr = new StreamReader(stream);
+            //写数据流对象
+            StreamWriter sw = new StreamWriter(stream);
+
+            while (true)
+            {
+                Console.WriteLine("客户端："+sr.ReadLine());
+                string msg = Console.ReadLine();
+                sw.WriteLine(msg); //刷新流
+                sw.Flush();
+            }
+
+            client.Close();//关闭客户端
+
+        }
+    }
+}
