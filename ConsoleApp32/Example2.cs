96938501d73d9b4149ae76673c9a4e84c98406fa
zhuchao <2580500887@qq.com>
2019-06-05 10:22:59 +0800

    判断一个数为2 的几次方

---
+++ ConsoleApp32/Example2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp32
+{
+    //判断一个数为2的几次方
+    class Example2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入要判断的数字");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int n = 1,i = 0;
+            while (n<num)
+            {
+                i++;
+                n  *= 2;
+                if (n==num)
+                {
+                    Console.WriteLine("该数字为2的{0}次方",i);
+                }
+                if (n>num)
+                {
+                    Console.WriteLine("该数字不为2的次方幂");
+                }
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
